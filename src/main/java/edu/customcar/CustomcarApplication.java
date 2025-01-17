d8c0ac137379737848d8ca5c3e9c0d1469eb6f69
Manuel Rico <manuelricolopez@liceolapaz.net>
2025-01-17 08:50:59 +0100

    Comenzando proyecto CustomCar, properties configurado y archivo data.sql finalizado

---
+++ src/main/java/edu/customcar/CustomcarApplication.java
@@ -0,0 +1,13 @@
+package edu.customcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/customcar/CustomcarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.customcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
